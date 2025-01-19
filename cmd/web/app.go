f5c87d9ce4cdb15fc68bd832265b3e74b0b061c5
Turtel216 <papadimitrios216@gmail.com>
2025-01-19 14:32:45 +0200

    added proper server shutdown

--- cmd/web/app.go
+++ cmd/web/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -36,10 +37,29 @@ func (app *App) Start(ctx context.Context) error {
 		return fmt.Errorf("Failed to connect to redis: %w", err)
 	}
 
-	err = server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
-	}
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
 
-	return nil
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
